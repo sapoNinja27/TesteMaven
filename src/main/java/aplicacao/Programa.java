326891a665976d31671c8bed8c42471dcc4576a9
Joao Pedro Ronzani dos Santos <joao.ronzani@betha.com.br>
2021-04-27 08:59:52 -0300

    'dao'

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,27 +1,75 @@
 package aplicacao;
 
-import javax.persistence.*;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
-//		Pessoa p1=new Pessoa("Nome1","email@gmail.com");
-//		Pessoa p2=new Pessoa("Nome2","email@gmail.com");
-//		Pessoa p3=new Pessoa("Nome3","email@gmail.com");
 		
-		EntityManagerFactory emf =Persistence.createEntityManagerFactory("exemplo-jpa");
-		EntityManager em= emf.createEntityManager();
-//		em.getTransaction().begin();
-//		em.persist(p1);
-//		em.persist(p2);
-//		em.persist(p3);
-//		em.getTransaction().commit();
-		Pessoa p= em.find(Pessoa.class, 2);
-		System.out.println(p);
+	}
+	public void incluir(Pessoa p) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
 		em.close();
 		emf.close();
+	}
+	public Pessoa buscar(int id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		Pessoa p = em.find(Pessoa.class, id);
+		em.close();
+		emf.close();
+		return p;
+	}
+	public void editar(int id,Pessoa np) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Pessoa p=em.find(Pessoa.class, id);
+		if(np.getNome()!=null) {
+			p.setNome(np.getNome());
+		}
+		if(np.getEmail()!=null) {
+			p.setEmail(np.getEmail());
+		}
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+	public void deletar(int id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Pessoa p=em.find(Pessoa.class, id);
+		em.remove(p);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+	public List<Pessoa> listar() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+	    CriteriaBuilder cb = em.getCriteriaBuilder();
+	    CriteriaQuery<Pessoa> pessoas = cb.createQuery(Pessoa.class);
+	    Root<Pessoa> rootEntry = pessoas.from(Pessoa.class);
+	    CriteriaQuery<Pessoa> all = pessoas.select(rootEntry);
+	    TypedQuery<Pessoa> allQuery = em.createQuery(all);
+		em.close();
+		emf.close();
+	    return allQuery.getResultList();
 	}
 
 }
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -4,42 +4,53 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
-public class Pessoa implements Serializable{
-	private static final long serialVersionUID=1L;
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 //	@Column(name="nomeCompleto")
 	private String nome;
 	private String email;
+
 	public Pessoa() {
-		
+
 	}
-	public Pessoa( String nome, String email) {
+
+	public Pessoa(Integer id, String nome, String email) {
+		super();
+		this.id = id;
 		this.nome = nome;
 		this.email = email;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	@Override
 	public String toString() {
 		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + "]";
 	}
-	
+
 }
